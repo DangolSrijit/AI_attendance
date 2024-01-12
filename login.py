bdb1eb378e3ec9c3e10d97e60b1c948891e6d7c6
Shrayashd <Shrayash.201639@ncit.edu.np>
2024-01-12 23:09:22 +0545

    login.py updatedgit add .!

--- login.py
+++ login.py
@@ -6,14 +6,16 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 import sqlite3
-import cv2
 import subprocess
-
-
-
+from attendance import Attendance
+import cv2
+import pickle
+import numpy as np
+import os
+from datetime import datetime
 
 app = Flask(__name__)
-
+camera = Attendance()
 
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -111,12 +113,11 @@ def register():
     return render_template('register.html', form=form)
 
 
-#@ app.route('/attendees_dashboard')
-#def attendees_dashboard():
-#   return render_template('attendees_dashboard.html')
+@ app.route('/attendees_dashboard')
+def attendees_dashboard():
+   return render_template('attendees_dashboard.html')
 
-#connect to DB
-conn = sqlite3.connect('database.db')
+
 
 @app.route('/')
 def index():
@@ -126,6 +127,87 @@ def index():
 def run_python_script():
      result = subprocess.check_output(['python', 'train.py'], universal_newlines=True)
      return result
+
+def generate_frames():
+    while True:
+        frame = camera.get_frame()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame_bytes = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def train():
+    
+
+    video=cv2.VideoCapture(0)
+    facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+
+    faces_data=[]
+
+    i=0
+
+    name=input("Enter Your Name: ")
+
+    while True:
+        ret,frame=video.read()
+        gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces=facedetect.detectMultiScale(gray, 1.3 ,5)
+        for (x,y,w,h) in faces:
+            crop_img=frame[y:y+h, x:x+w, :]
+            resized_img=cv2.resize(crop_img, (50,50))
+            if len(faces_data)<=100 and i%10==0:
+                faces_data.append(resized_img)
+            i=i+1
+            cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 1)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
+        cv2.imshow("Frame",frame)
+        k=cv2.waitKey(1)
+        if k==ord('q') or len(faces_data)==100:
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
+    faces_data=np.asarray(faces_data)
+    faces_data=faces_data.reshape(100, -1)
+
+
+    if 'names.pkl' not in os.listdir('data/'):
+        names=[name]*100
+        with open('data/names.pkl', 'wb') as f:
+            pickle.dump(names, f)
+    else:
+        with open('data/names.pkl', 'rb') as f:
+            names=pickle.load(f)
+        names=names+[name]*100
+        with open('data/names.pkl', 'wb') as f:
+            pickle.dump(names, f)
+
+    if 'faces_data.pkl' not in os.listdir('data/'):
+        with open('data/faces_data.pkl', 'wb') as f:
+            pickle.dump(faces_data, f)
+    else:
+        with open('data/faces_data.pkl', 'rb') as f:
+            faces=pickle.load(f)
+        faces=np.append(faces, faces_data, axis=0)
+        with open('data/faces_data.pkl', 'wb') as f:
+            pickle.dump(faces, f)
+            
+def generate_frames_1():
+    while True:
+        frame = camera.get_frame()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame_bytes = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+
+@app.route('/video_train')
+def video_train():
+    return Response(generate_frames_1(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 
    
